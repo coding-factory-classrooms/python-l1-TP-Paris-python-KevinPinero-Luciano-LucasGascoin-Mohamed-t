955a8ec919d4cb4890b14e26983c6e48b0232525
kevin <pinerokevin@icloud.com>
2021-11-17 21:27:19 +0100

    gestion des erreurs try/catch

--- core.py
+++ core.py
@@ -1,3 +1,6 @@
+from os import listdir
+from os.path import isfile, join
+
 import cv2
 import os
 from os.path import basename
@@ -8,6 +11,8 @@ from filters import dilate
 from filters import blur
 
 
+#list_imgs = [f for f in listdir("Data/imgs/") if isfile(join("Data/imgs", f))]
+#src = ("Data/imgs/img_test.png")
 
 src = ("Data/imgs/img_test.png")
 
@@ -15,22 +20,36 @@ def read_image(src):
     image = cv2.imread(src)
     return image
 
-output_gray = gray.gray(read_image(src))
-output_blur = blur.blur(output_gray, 35)
-output_dilate = dilate.dilate(output_blur, 20)
+try:
+    output_gray = gray.gray(read_image(src)) # ceci permet de pouvoir ouvrir le fichier ou non
+except:
+    pass
+    #print("le fichieeerezrze")
+try:
+    output_blur = blur.blur(output_gray, -40)
+    output_dilate = dilate.dilate(output_blur, -20)
+except:
+    pass
+    #print(f"le fichier  {src} n'existe pas")
 
 
 def write():
-    cv2.imwrite(f'output/filtre_{basename(src)}', output_dilate)
+    try:
+        cv2.imwrite(f'output/filtre_{basename(src)}', output_dilate)
+    except:
+        print(f"le fichier {src} n'est pas une image ou n'existe pas ou vous avez appliqué un flou négatif ou pair")
 
 
 
 write()
 
 #test pour afficher l'image si bueno
-cv2.imshow('read', output_dilate)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+try:
+    cv2.imshow('read', output_dilate)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+except:
+    print(f"le print du test ne fonctionne pas pour {src}")
 #-----------------------------------------
 
 
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import core
+from os import listdir
+from os.path import isfile, join
 
 
+#
+# list_imgs = [f for f in listdir("Data/imgs/") if isfile(join("Data/imgs", f))]
+# path = '/Data/imgs'
+# img = "/Data/imgs/"
+#
+# print(path)
+# #FichList = [ f for f in path if isfile(join('.',f)) ]
+# #print(FichList)
+# print(fichiers)
+
+# def target_imgs():
+#     for i in
 
 
 
